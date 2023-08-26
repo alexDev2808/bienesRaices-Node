2c5518d7a4c0cea23ea0676c17603925ba68e577
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-26 17:27:51 -0600

    Probando peticiones

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res) {
     res.send('Hola Mundo en express')
 });
 
+router.post('/', function(req, res) {
+    res.json({msg: "Node JSON"})
+});
+
+
+
 router.get('/nosotros', function(req, res) {
     res.send('Hola Mundo en express. NOSOTROS')
 });
