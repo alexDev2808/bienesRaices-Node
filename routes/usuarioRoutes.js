77bf7ca007867d10073d91a5d10b9ae78224915e
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-26 17:52:41 -0600

    Implementando MVC

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,19 +1,13 @@
 import express from "express";
+import { formularioLogin, formularioRegistro } from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 // Routing
 
-router.get('/login', function(req, res) {
-    res.render('auth/login', {
-        autenticado: true
-    })
-});
+router.get('/login', formularioLogin);
+router.get('/registro', formularioRegistro)
 
 
-
-router.get('/nosotros', function(req, res) {
-    res.send('Hola Mundo en express. NOSOTROS')
-});
 
 export default router
