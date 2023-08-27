a243839d7fd9aeead2eb7fcd0ea7e30b361e5ea8
J. Alexis TH <colores.2808.8000@gmail.com>
2023-08-26 19:45:40 -0600

    Creando la db, sequelize y .env

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import usuarioRoutes from './routes/usuarioRoutes.js';
+import db from './config/db.js';
 
 // Crear la app
 const app = express();
+
+
+// Conexion a la db
+try {
+    await db.authenticate()
+    console.log("Conexion correcta a la base de datos");
+} catch(error) {
+    console.log(error);
+}
 
 // Habiliitar Pug
 
